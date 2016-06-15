09ce03c9c67b539cf26dd6910299b12a0798dc7f
Hea3veN <hea3venmc@gmail.com>
2016-06-15 10:54:15 -0300

    Clean up the branch

--- src/main/java/com/hea3ven/malleable/item/ItemMetalPickaxe.kt
+++
@@ -1,7 +0,0 @@
-package com.hea3ven.malleable.item
-
-import com.hea3ven.malleable.metal.Metal
-import net.minecraft.item.*
-
-class ItemMetalPickaxe(override val metal: Metal) : ItemPickaxe(metal.toolMaterial), ItemMetalSingle {
-}
--- src/main/java/com/hea3ven/malleable/item/ItemMetalShovel.kt
+++
@@ -1,7 +0,0 @@
-package com.hea3ven.malleable.item
-
-import com.hea3ven.malleable.metal.Metal
-import net.minecraft.item.*
-
-class ItemMetalShovel(override val metal: Metal) : ItemSpade(metal.toolMaterial), ItemMetalSingle {
-}
